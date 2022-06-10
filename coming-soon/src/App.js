be25cbbcd5e97547e548abb18b91acd79e512b68
Elameen123 <69402639+Elameen123@users.noreply.github.com>
2022-06-10 19:09:13 +0100

    Layout

---
+++ coming-soon/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Landing from './Landing_page/Landing';
+
+function App() {
+  return (
+    <div className="App">
+      {/* Landing Page  */}
+      <Landing></Landing>
+    </div>
+  );
+}
+
+export default App;
---
+++ coming-soon/src/Landing_page/Landing.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import Text1 from '../components/Text1/Text1'
+import Text2 from '../components/Text2/Text2'
+import Handles from '../components/Handles/Handles'
+import Newsletter from '../components/NewsLetter/Newsletter'
+import Timer from '../components/Timer/Timer'
+import './Landing.css'
+
+
+const Landing = () => {
+  return (
+    <div className='Container'>
+      <div className='container-div'>
+        {/* Logo  */}
+        <div className='logo'>
+
+        </div>
+
+        {/* Text1  */}
+        <div className='Text1'>
+          <Text1></Text1>
+        </div>
+
+        {/* Timer  */}
+        <div className='Timer'>
+          <Timer></Timer>
+        </div>
+
+        {/* Text2  */}
+        <div className='Text2'>
+          <Text2></Text2>
+        </div>
+
+        {/* Handles  */}
+        <div className='Handles'>
+          <Handles></Handles>
+        </div>
+
+        {/* NewsLetter  */}
+        <div className='Newsletter'>
+          <Newsletter></Newsletter>
+        </div>
+      </div>
+
+      <div className='container-div'>
+
+      </div>
+    </div>
+  )
+}
+
+export default Landing
